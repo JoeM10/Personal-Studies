d21a2c87e0d33be034c6428e172cc081bcad5aec
Jacob Thornton <jacobreidth@hotmail.com>
2023-06-12 17:11:38 -0700

    Made vehicle stuff

---
+++ Vehicles/car.py
@@ -0,0 +1,4 @@
+import vehicle
+
+class car(vehicle):
+    
---
+++ Vehicles/vehicle.py
@@ -0,0 +1,9 @@
+class vehicle(object):
+    def __init__(self, property1, property2, property3):
+        self.property1 = property1
+        self.property2 = property2
+        self.property3 = property3
+
+    # Getters
+    def getProperty1(self):
+        return self.property1
